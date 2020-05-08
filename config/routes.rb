1383a702d9003dc8587d2ff2bade3d94fd237df2
bettercallazamat <bettercallazamat@gmail.com>
2020-05-09 02:56:16 +0600

    Add tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Blogger::Application.routes.draw do
-  resources :articles
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
-  root to: 'articles#index'
+  resources :tags
 end
